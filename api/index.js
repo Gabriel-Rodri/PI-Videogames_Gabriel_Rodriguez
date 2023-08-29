537db1e7650984808f647d11e8f68973aed04da3
Gabriel-Rodri <116038089+Gabriel-Rodri@users.noreply.github.com>
2023-08-29 19:39:02 -0300

    V1.0.6_fix_db_variables

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-
+require ('dotenv').config()
+const {PORT} = process.env
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Sincronizando todos los modelos a la vez.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); 
   });
 });
