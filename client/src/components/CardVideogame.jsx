a947e87df472f10d70b7fd364950b30c08bff228
Gabriel-Rodri <116038089+Gabriel-Rodri@users.noreply.github.com>
2023-08-29 17:46:37 -0300

    ;

--- client/src/components/CardVideogame.jsx
+++ client/src/components/CardVideogame.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from '../style/CardVideogame.module.css'
+import s from '../style/CardVideogame.module.css';
 import { NavLink } from "react-router-dom";
 
 class CardVideogame extends React.Component {
