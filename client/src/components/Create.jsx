c8786101f0e2e3ce0fd4e5858c0c7aae8eb305f8
Gabriel-Rodri <116038089+Gabriel-Rodri@users.noreply.github.com>
2023-08-30 00:51:28 -0300

    Update Create.jsx

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -5,46 +5,7 @@ import { createVideogame, getByGenres, getPlatforms } from "../redux/actions";
 import s from '../style/Create.module.css'
 import { NavLink } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'
-
-
-function validate (input) {
-  let errors = {}
-
-  if(!input.name) {
-    errors.name = 'Name is required'
-  } else if(!/^[a-zA-Z0-9-() .]+$/.test(input.name)){
-    errors.name = 'Only letters, numbers, hyphens, and parentheses are accepted.'
-  }
-
-  if(input.image.length !== 0 && !/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/.test(input.image)){
-    errors.image='invalid URL'
-  }
-
-  if (!input.description) {
-    errors.description = 'Description is required';
-} else if (input.description.length > 100) {
-    errors.description = 'The description is very long. (Max = 100 characters)';
-} else if (/\s{2,}/.test(input.description)) {
-    errors.description = 'The description must not contain successive white spaces.';
-} else if (input.description.length < 100) {
-  errors.description = 'The description is very short. (Min = 10 characters)';
-}
-
-
-  if(!input.released) {
-    errors.released = 'The release date is required'
-  }
-
-  if(!input.rating) {
-    errors.rating = 'The rating is required'
-  } else if(input.rating > 5) {
-    errors.rating = 'The rating should not be higher than 5'
-  } else if(input.rating < 0) {
-    errors.rating = 'The rating cannot be a negative number'
-  }
-
-  return errors 
-}
+import validate from "./validations/validate";
 
 export default function Create() {
     const [input, setInput] = useState({
